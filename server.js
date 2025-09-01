b361d67d942bed71c40c3809754b44d8a0e099f6
Sithembiso Masoka <sfmasoka@users.noreply.github.com>
2025-09-01 14:35:26 +0200

    Update

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ app.use(cors());
 const adminRoutes = require("./src/routes/admin.routes");
 app.use("/admin", adminRoutes);
 
-const studentRoutes = require("./src/routes/student.routes");
-app.use("/student", studentRoutes);
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
